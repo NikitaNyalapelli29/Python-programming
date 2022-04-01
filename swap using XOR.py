3e5e3119c4e04135b15375d27f15ceceaf29eadd
Nikita Nyalapelli <85953042+NikitaNyalapelli29@users.noreply.github.com>
2022-04-01 07:24:29 +0530

    Create swap using XOR.py

---
+++ swap using XOR.py
@@ -0,0 +1,11 @@
+x = 10
+y = 50
+print("Before Swapping")
+print("Value of x: ",x)
+print("Value of y: ",y)
+x = x^y
+y = x^y
+x = x^y
+print("After Swapping")
+print("Value of x: ",x)
+print("Value of y: ",y)
