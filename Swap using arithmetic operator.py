c3ca942a1b9bfbfd5e109ff061d802ec39e189f3
Nikita Nyalapelli <85953042+NikitaNyalapelli29@users.noreply.github.com>
2022-04-01 07:22:24 +0530

    Create Swap using arithmetic operator.py

---
+++ Swap using arithmetic operator.py
@@ -0,0 +1,11 @@
+x = 20
+y = 80
+print("Before Swapping")
+print("Value of x: ",x)
+print("Value of y: ",y)
+x = x + y
+y = x - y
+x = x - y
+print("After Swapping")
+print("Value of x: ",x)
+print("Value of y: ",y)
