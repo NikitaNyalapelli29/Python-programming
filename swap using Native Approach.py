b454c7bc785a65f0304ae6d52bc7585832affa07
Nikita Nyalapelli <85953042+NikitaNyalapelli29@users.noreply.github.com>
2022-04-01 07:19:26 +0530

    Update swap using Native Approach.py

--- swap using Native Approach.py
+++ swap using Native Approach.py
@@ -1,7 +1,12 @@
 x=20
 y=30
+print("Before Swapping")
+print("Value of x: ",x)
+print("Value of y: ",y)
 temp = x
 x = y
 y = temp
+print("After Swapping")
 print("Value of x: ",x)
 print("Value of y: ",y)
+
